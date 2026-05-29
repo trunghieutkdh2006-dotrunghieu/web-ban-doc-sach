7710d182f0c8a7f049801c8e1e91dcb4c252324a
Đỗ Trung Hiếu <dotrunghieu0705gmail.com@MacBook-Pro-cua-o.local>
2026-05-29 22:53:04 +0700

    update css

--- backend/models/User.js
+++ backend/models/User.js
@@ -13,4 +13,4 @@ const userSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
